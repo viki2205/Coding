aec3071ac12d0b1bdd07d958c0b758c49c77e717
Vignesh <36230260+viki2205@users.noreply.github.com>
2018-03-24 17:45:16 +0530

    Add files via upload

---
+++ Nested loops 2.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main()
+{
+	int n,a[100],i,j,temp;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+			}
+		}
+	}
+	printf("\nWeakest first:");
+	for(i=0;i<n;i++)
+	{
+	    printf("%d\t",a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]<a[j])
+			{
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+			}
+		}
+	}
+	printf("\nStrongest first:");
+	for(i=0;i<n;i++)
+	{
+	    printf("%d\t",a[i]);
+	}
+	return 0;
+}
